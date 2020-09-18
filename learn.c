142377fb5fc5cb1c5d9291ef49c019db167f9eda
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-09-18 06:57:02 +0000

    Add read user input feature

--- learn.c
+++ learn.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BUFFER_SIZE 3000
 #define TOKENS_SIZE 50
 
 int main(int argc, char **argv){
+    
+    /*max number of characters for input */
     char buffer[BUFFER_SIZE];
 
+    /*max number of characters for each token */
     char *tokens[TOKENS_SIZE];
     int num_tokens = 0;
-
+    
+    /*token pointer */
     char *token_ptr;
     
-    fgets(tokens, TOKENS_SIZE, stdin);
-    printf("%s\n",tokens);
+    /*Read user input */
+    fgets(buffer, BUFFER_SIZE, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+    printf("%s\n",buffer);
     
 }
