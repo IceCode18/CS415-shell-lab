ab17a056d0c9ce38080f973862b39e50180c6094
Jaykant Tiano <jtiano19@go.byuh.edu>
2020-09-18 06:25:43 +0000

    Add boilerplate

---
+++ learn.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stlib.h>
+
+#define BUFFER_SIZE = 3000
+#define TOKENS_SIZE = 50
+
+int main(int argc, char **argv){
+    char buffer[BUFFER_SIZE];
+
+    char *tokens[TOKENS_SIZE];
+    int num_tokens = 0;
+
+    char *token_ptr;
+    
+    
+    
+}
